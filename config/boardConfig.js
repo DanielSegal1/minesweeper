0120e9c89af6ca16dd53598316b011413325fcf7
Daniel Segal <danielsegal909@gmail.com>
2021-04-15 17:31:35 +0300

    created config for level options

---
+++ config/boardConfig.js
@@ -0,0 +1,14 @@
+const LEVELS = {
+    'Easy': {
+        size: 8,
+        minesQuantity: 8
+    },
+    'Medium': {
+        size: 12,
+        minesQuantity: 35
+    },
+    'Hard': {
+        size: 16,
+        minesQuantity: 50
+    }
+}
